074471821bfbebe7b14bc3c803472f0fb0492b30
Lars Hupel <lars.hupel@mytum.de>
2023-12-08 21:28:17 +0100

    highly experimental ics file

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source "https://rubygems.org"
 
 gem "jekyll", ">=4"
+gem "icalendar"
+gem "tzinfo"
 
 group :jekyll_plugins do
   gem "jekyll-sitemap"
---
+++ _plugins/events.rb
@@ -0,0 +1,52 @@
+require "icalendar"
+require "digest"
+require "time"
+require "tzinfo"
+require "icalendar/tzinfo"
+
+module Jekyll
+  class QTiesEventGenerator < Jekyll::Generator
+    def generate(site)
+      @tzid = "Europe/Berlin"
+      @time_zone = TZInfo::Timezone.get(@tzid)
+      ics = PageWithoutAFile.new(site, __dir__, "", "events.ics")
+      ics.content = calendar(site.config["url"], site.collections["events"].docs)
+      ics.data["layout"] = nil
+      site.pages << ics
+    end
+
+    private def date_with_time(date, time)
+      time = Time.strptime("#{date} #{time}", "%Y-%m-%d %H:%M")
+      time = @time_zone.utc_to_local(time.utc)
+      time
+    end
+
+    private def calendar(site_url, events)
+      cal = Icalendar::Calendar.new
+      cal.add_timezone(@time_zone.ical_timezone(DateTime.new 2022, 1, 1, 0, 0, 0))
+
+      events.each do |event|
+        event.data["dates"].each do |date|
+          e = cal.event
+          long_id = "#{date["day"]}-#{event.id}"
+          e.uid = Digest::SHA2.hexdigest long_id
+          e.summary = event.data["title"]
+          url = site_url + event.url
+          e.url = url
+          e.description = "#{event.data["description"]}\nAlle Details auf #{url}"
+          e.ip_class = "PUBLIC"
+          e.organizer = "mailto:info@qties-muenchen.de"
+          e.location = event.data["place"]
+
+          start_time = date_with_time(date["day"], date["start"])
+          end_time = date_with_time(date["day"], date["end"])
+
+          e.dtstart = Icalendar::Values::DateTime.new start_time, 'tzid' => @tzid
+          e.dtend = Icalendar::Values::DateTime.new end_time, 'tzid' => @tzid
+        end
+      end
+      cal.publish
+      cal.to_ical
+    end
+  end
+end
